320916cbee62648d22933d914ec1c6f80e0848d8
Rell Credle <ter@rel.lc>
2024-09-16 07:15:29 -0400

    Update setup_postgres.sh

--- setup_postgres.sh
+++ setup_postgres.sh
@@ -2,12 +2,12 @@
 
 service postgresql start
 
-echo "host all all 172.17.0.0/16 md5" >>/etc/postgresql/12/main/pg_hba.conf
-echo "listen_addresses='*'" >>/etc/postgresql/12/main/postgresql.conf
+echo "host all all 0.0.0.0/0 md5" >> /etc/postgresql/12/main/pg_hba.conf
+echo "listen_addresses='*'" >> /etc/postgresql/12/main/postgresql.conf
 
 service postgresql restart
 
-gosu postgres psql -c "CREATE USER man_user WITH PASSWORD 'Y8ksKX2uqdHEepzW8s9*vX@LbANPVbrQgfgzpRgP@dJATFKCfQ6de@n3g6GYeL-yrh3Mp!CKa-hQdUM';"
-gosu postgres psql -c "CREATE DATABASE man_db WITH OWNER man_user;"
+gosu postgres psql -c "CREATE USER ${DJANGO_DB_USER} WITH PASSWORD '${DJANGO_DB_PASS}';"
+gosu postgres psql -c "CREATE DATABASE ${DJANGO_DB_NAME} WITH OWNER ${DJANGO_DB_USER};"
 
-gosu postgres psql -d man_db -c "CREATE EXTENSION postgis; CREATE EXTENSION postgis_topology;"
+gosu postgres psql -d ${DJANGO_DB_NAME} -c "CREATE EXTENSION postgis; CREATE EXTENSION postgis_topology;"
