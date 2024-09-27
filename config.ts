5ad19847536c03cbd7833deb4cd0f92402b948ba
Rell Credle <ter@rel.lc>
2024-09-27 12:01:14 -0400

    new

---
+++ config.ts
@@ -0,0 +1,8 @@
+/**
+ * All the configuration variables are stored here
+ top is local 
+ second is prod
+ */
+const API_BASE_URL = "http://localhost:8000";
+// const API_BASE_URL = "http://ec2-3-15-183-130.us-east-2.compute.amazonaws.com:8000";
+export default API_BASE_URL;
