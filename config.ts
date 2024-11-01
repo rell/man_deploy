97ca758ed1194ff4a71a5b7496c0799db88217cb
Rell Credle <ter@rel.lc>
2024-11-01 15:02:48 -0400

    new config.ts

--- config.ts
+++ config.ts
@@ -3,6 +3,6 @@
  top is local 
  second is prod
  */
-const API_BASE_URL = "http://128.183.160.250:8000";
+const API_BASE_URL = "http://aeronet.gsfc.nasa.gov/new_web/manviewer/api";
 // const API_BASE_URL = "http://ec2-3-15-183-130.us-east-2.compute.amazonaws.com:8000";
 export default API_BASE_URL;
